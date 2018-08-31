eafc9bb0a9eacff310bc1b908e17d676887d4855
Anant Gupta <gupanant@gmail.com>
2018-08-30 17:19:22 -0700

    React Course - Udemy

---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person" style={style}>
+            <p onClick={props.click}>I'm a {props.name}! I am {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
+};
+
+export default person;
